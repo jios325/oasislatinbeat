7e2d66528a38df863d7150ce932b5ead5916663a
MaxiTec <mtec@oasishoteles.com>
2016-06-23 17:58:47 -0500

    Interna

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,28 +1,205 @@
    var myCenter = new google.maps.LatLng(21.162746, -86.853439);
 
     function initialize() {
-           var mapProp = {
+           var mapProp = {//propiedades del MAPA
             center:myCenter,
             zoom:16,
+            scrollwheel:false,
             mapTypeId:google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp); 
         var marker = new google.maps.Marker({
             position: myCenter,
             map:map,
-            title:"Ecowhite Smart Laundry"
+            title:"Ubicación Aquí"
         });
-         var contentString = '<div id="content">'+
-     	'<h1>Ecowhite Smart Laundry</h1>'+
-     	'<img style="margin:o auto;"src="images/logo-ecowhite.png" alt="" />'
-      	'</div>';
+      //    var contentString = '<div id="content">'+
+     	// '<h1>Titulo del Marker</h1>'+
+     	// '<img style="margin:o auto;"src="images/logo-ecowhite.png" alt="" />'
+      // 	'</div>';
 
-	  var infowindow = new google.maps.InfoWindow({
-	    content: contentString
-	  });
+	   //  var infowindow = new google.maps.InfoWindow({
+	   //  content: contentString
+	   // });
 		marker.setMap(map)
-		  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-		;}
-        google.maps.event.addDomListener(window, 'load', initialize)
+		marker.addListener('click', function() {
+        // infowindow.open(map, marker);
+        });
+        map.set('styles', [
+          {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+        ]);
+	;}
+    google.maps.event.addDomListener(window, 'load', initialize)
